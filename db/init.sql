cb774c700c9a158f34b22d73c2db27b3b949eeee
nhoshino028 <n.hoshino028@gmail.com>
2026-06-01 17:27:59 +0900

    init.sqlの修正

--- db/init.sql
+++ db/init.sql
@@ -9,7 +9,7 @@ CREATE TABLE reminders (
     updated_at timestamptz not null default now()
 );
 
-INSERT INTO reminders (id, title, comment, remind_at, notify_email, is_notified)
+INSERT INTO reminders (title, comment, remind_at, notify_email, is_notified)
 VALUES
-    ('001', '帰りに〇〇へ行く', '〇分前には会社を出る', '2026-06-01 18:00:00', 'sample@sample.co.jp', '1'),
-    ('002', '〇〇を買う', NULL, '2026-06-02 9:00:00', 'sample@sample.co.jp', '0');
+    ('帰りに〇〇へ行く', '〇分前には会社を出る', '2026-06-01 18:00:00+09', 'sample@sample.co.jp', '1'),
+    ('〇〇を買う', NULL, '2026-06-02 09:00:00+09', 'sample@sample.co.jp', '0');
